eefaf3300aceb2704a94af01b773042b5622b323
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-02 15:17:37 +0545

    fix: change base api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 
 // Axios instance for API calls with cookies
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_BASE = import.meta.env.VITE_SERVER_URL;
 
 const api = axios.create({
   baseURL: API_BASE,
