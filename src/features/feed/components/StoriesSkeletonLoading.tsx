7f731a793dca064990cf7a4b026eb158897068df
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-03 17:28:28 +0545

    fix: decrease size of story in mobile view

--- src/features/feed/components/StoriesSkeletonLoading.tsx
+++ src/features/feed/components/StoriesSkeletonLoading.tsx
@@ -1,12 +1,12 @@
 function StoriesSkeletonLoading() {
   return (
     <div className="flex w-full gap-3 px-5 lg:gap-5 lg:px-25">
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500"></div>
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500"></div>
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:h-20 lg:w-20"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:h-20 lg:w-20"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:h-20 lg:w-20"></div>
+      <div className="hidden: h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex lg:h-20 lg:w-20"></div>
+      <div className="hidden: h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex lg:h-20 lg:w-20"></div>
+      <div className="hidden: h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-500 lg:flex lg:h-20 lg:w-20"></div>
     </div>
   );
 }
