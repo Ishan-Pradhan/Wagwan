b4aae8ce59c74589601ca43d1969badf936037d6
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-05 11:48:27 +0545

    feat: change skeleting color according to theme

--- src/features/message/components/SkeletonLoading.tsx
+++ src/features/message/components/SkeletonLoading.tsx
@@ -1,14 +1,14 @@
 function SkeletonLoading() {
   return (
     <div className="relative flex w-full gap-4">
-      <div className="h-10 w-10 animate-pulse rounded-full bg-gray-600" />
+      <div className="h-10 w-10 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600" />
 
       <div className="flex flex-col gap-2">
         {/* Username of the receiver */}
-        <div className="h-5 w-15 animate-pulse rounded-full bg-gray-600"></div>
+        <div className="h-5 w-15 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
 
         {/* Last message */}
-        <p className="h-4 w-20 animate-pulse rounded-full bg-gray-600"></p>
+        <p className="h-4 w-20 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></p>
       </div>
     </div>
   );
