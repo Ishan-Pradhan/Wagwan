228a206ae04c4bb5ae16f972e71717ea252600ef
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-06 11:59:58 +0545

    fix: loading story height

--- src/features/feed/components/StoriesSkeletonLoading.tsx
+++ src/features/feed/components/StoriesSkeletonLoading.tsx
@@ -1,12 +1,12 @@
 function StoriesSkeletonLoading() {
   return (
     <div className="flex w-full gap-3 px-5 lg:gap-5 lg:px-25">
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
-      <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
-      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
-      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
-      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 dark:bg-gray-600"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 dark:bg-gray-600"></div>
+      <div className="h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 dark:bg-gray-600"></div>
+      <div className="hidden h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 xl:flex dark:bg-gray-600"></div>
+      <div className="hidden h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 xl:flex dark:bg-gray-600"></div>
+      <div className="hidden h-15 w-15 shrink-0 animate-pulse rounded-full bg-gray-400 lg:h-20 lg:w-20 xl:flex dark:bg-gray-600"></div>
     </div>
   );
 }
