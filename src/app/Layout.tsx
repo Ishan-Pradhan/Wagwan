38287dc92b93ea2294e5bb8e560f04edb22e5679
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-06 11:30:35 +0545

    feat: make side menu responsive to large screens

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -4,14 +4,14 @@ import Header from "@components/Layout/Header";
 
 function Layout() {
   return (
-    <div className="grid h-screen grid-cols-1 lg:grid-cols-6">
+    <div className="grid h-screen grid-cols-1 lg:grid-cols-15 xl:grid-cols-6">
       <div className="fixed top-0 z-10 w-full lg:hidden">
         <Header />
       </div>
       <div className="fixed bottom-0 z-10 order-2 w-full items-center justify-center overflow-hidden lg:relative lg:order-1 lg:col-span-1">
         <SideMenu />
       </div>
-      <div className="order-1 col-span-1 mt-18 overflow-y-auto lg:order-2 lg:col-span-5 lg:mt-0">
+      <div className="order-1 col-span-1 mt-18 overflow-y-auto lg:order-2 lg:col-span-14 lg:mt-0 xl:col-span-5">
         <Outlet />
       </div>
     </div>
