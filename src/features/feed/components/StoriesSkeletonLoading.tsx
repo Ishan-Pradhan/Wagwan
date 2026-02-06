6f45515f053d0db5be7f8d5e8816bca562aceaf0
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-06 11:58:22 +0545

    fix: fix skeleton loading of stories and fix alignment of button loading

--- src/features/feed/components/StoriesSkeletonLoading.tsx
+++ src/features/feed/components/StoriesSkeletonLoading.tsx
@@ -4,9 +4,9 @@ function StoriesSkeletonLoading() {
       <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
       <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
       <div className="h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 lg:flex dark:bg-gray-600"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 lg:flex dark:bg-gray-600"></div>
-      <div className="hidden: h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 lg:flex dark:bg-gray-600"></div>
+      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
+      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
+      <div className="hidden h-20 w-20 shrink-0 animate-pulse rounded-full bg-gray-400 xl:flex dark:bg-gray-600"></div>
     </div>
   );
 }
