70e79911ff665c989139ba54d58f7d7f6625156d
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-11 15:09:07 +0545

    fix: fix user not being able to login with google

--- src/app/protectedRoutes/requireLogin.ts
+++ src/app/protectedRoutes/requireLogin.ts
@@ -1,7 +1,18 @@
 import { redirect } from "react-router";
 
 // we can't use tanstack query in here because this is loader based route guard
-export default async function requireLogin() {
+export default async function requireLogin({ request }: { request: Request }) {
+  const url = new URL(request.url);
+  const accessToken = url.searchParams.get("accessToken");
+  const refreshToken = url.searchParams.get("refreshToken");
+
+  if (accessToken && refreshToken) {
+    document.cookie = `accessToken=${accessToken}; path=/; secure; samesite=none`;
+    document.cookie = `refreshToken=${refreshToken}; path=/; secure; samesite=none`;
+
+    return redirect(url.pathname);
+  }
+
   const persistRoot = localStorage.getItem("persist:root");
   const auth = persistRoot ? JSON.parse(JSON.parse(persistRoot).auth) : null;
   if (!auth.user) return redirect("/login");
