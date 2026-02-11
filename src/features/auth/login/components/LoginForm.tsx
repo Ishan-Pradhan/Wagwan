b079a6f4f078f1b07c53140357967684ed01c1ea
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-11 14:52:50 +0545

    fix: fix google callback url for production and fix padding in search

--- src/features/auth/login/components/LoginForm.tsx
+++ src/features/auth/login/components/LoginForm.tsx
@@ -61,7 +61,7 @@ function LoginForm() {
   };
 
   const handleGoogleButton = () => {
-    window.location.href = "http://localhost:8080/api/v1/users/google";
+    window.location.href = `${import.meta.env.VITE_SERVER_URL}/api/v1/users/google`;
   };
 
   return (
